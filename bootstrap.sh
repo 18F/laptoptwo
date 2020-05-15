97652cf97f4584eb87ea40cc3cefdbe6411200a2
Matt Jadud <matthew.jadud@gsa.gov>
2020-05-15 09:08:19 -0400

    Reworking to be more fully committed to Ansible.
    Minimizing work done outside of Ansible.
    
    Also, a lot more use of temp space. Instead of assuming the laptop was
    setup before, I always assume the laptop has not been setup. Therefore,
    a fresh pip is installed into the temp space, ansible is pulled, and
    automation commences. Likewise, Homebrew is always checked, because I
    need git.
    
    There may be a way to speed up re-runs, but really, for now, I'd rather
    assume the worst of the target machine, and go from there.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,45 +4,102 @@ ORG=jadudm
 REPOS=laptoptwo
 ORG_REPOS=${ORG}/${REPOS}
 
-# My goal is to do as little in bash/shell as possible.
-pushd /tmp
-    curl --remote-name https://raw.githubusercontent.com/${ORG_REPOS}/master/support/setup_shell.sh
-    curl --remote-name https://raw.githubusercontent.com/${ORG_REPOS}/master/support/brew.sh
-    curl --remote-name https://raw.githubusercontent.com/${ORG_REPOS}/master/support/python.sh
-    # First, do the shell setup dance.
-    source setup_shell.sh
-    # To bootstrap, we want to get to Ansible as quickly as possible.
-    # This suggests installing Python 3 (via Homebrew) and then
-    # pip installing Ansible. From there, use Ansible for automation.
-    source brew.sh
-    source python.sh
+# GOAL
+# Use Ansible for systems automation, not Bash.
+# Therefore, my goal is to get to Ansible as quickly as possible.
 
-    # I'm going to need 'git' to bootstrap.
-    if command -v git >/dev/null 2>&1; then
+# install_homebrew :: None -> None
+# PURPOSE
+# Installs homebrew.
+install_homebrew () {
+    if ! command -v brew > /dev/null; then
+        printf "[LAPTOP] Installing the Homebrew package manager."
+        printf "[LAPTOP] You will probably be prompted for your password."
+        ruby -e "$(curl --location --fail --silent --show-error https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        # This will be local to this script for now; Ansible will set the shell
+        # variable properly once we're bootstrapped.
+        export PATH="/usr/local/bin:$PATH"
+    else
+        printf "[LAPTOP] Update Homebrew\n"
+        brew update
+    fi
+    printf "\n"    
+}
+
+# install_git :: None -> None
+# PURPOSE
+# What it says on the tin. Everything is easier if we have 
+# git installed. 
+install_git () {
+    echo "[LAPTOP] Checking for git."
+    if ! command -v git > /dev/null; then
         brew install git
     fi
-popd
+}
 
-# At this point, Ansible should be installable.
-# 'wheel' cuts down on warnings in the subsequent installs.
-pip install --upgrade pip
-pip install wheel ansible
+# setup_tmp_dir :: None -> None
+# PURPOSE
+# Creates a temporary directory in the user's temp space.
+# On macOS, mktemp doesn't behave the same as Linux. Beware.
+setup_tmp_dir () {
+    # See
+    # https://unix.stackexchange.com/questions/30091/fix-or-alternative-for-mktemp-in-os-x
+    # for why this is necessary.
+    echo "[LAPTOP] Creating a temporary directory."
+    echo "[LAPTOP] This is small, and will disappear on reboot."
+    DIRNAME=laptop-`date +%s`
+    export TMP_DIR=$(mktemp -d -t $DIRNAME)
+}
 
-pushd /tmp
-    # Get the repository. This lets us update the Ansible scripts
-    # arbitrarily, and know that, on re-run, we always get the most
-    # recent version of the configuration/automation.
-    if [[ -d ${REPOS} ]]; then
-        pushd ${REPOS}
-            git pull
+# update_user_pip :: None -> None
+# PURRPOSE
+# We always install a pip. Never trust what is local.
+# This is slower, arguably, but provides consistency.
+get_pip () { 
+    if ! command -v pip3 > /dev/null; then
+        echo "[LAPTOP] Installing a temporary pip for automation."
+        pushd ${TMP_DIR}
+            export PIP_TARGET=${TMP_DIR}/pip
+            mkdir -p ${PIP_TARGET}
+            curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+            python get-pip.py --upgrade --no-cache-dir --no-warn-script-location
         popd
-    else
-        git clone https://github.com/${ORG_REPOS}
     fi
+}
 
-    pushd ${REPOS}
-        # At this point, we have the repository, and can 
-        # run the most up-to-date playbook.
-        ansible-playbook -i hosts playbook.yaml
-    popd
-popd
+# setup_virtual_environment :: None -> None
+# PURPOSE
+# Sets up a virtual environment in /tmp so that we can have a 
+# known Python3 to work with. 
+setup_virtual_environment () {
+    echo "[LAPTOP] Setting up a venv."
+    THIS_VENV=${TMP_DIR}/laptop-setup-venv
+    pip3 install --no-cache-dir --upgrade virtualenv
+    virtualenv --system-site-packages -p python3 ${THIS_VENV}
+    echo "Activating virtualenv: ${THIS_VENV}"
+    source ${THIS_VENV}/bin/activate
+}
+
+# pip_install_ansible :: None -> None
+# PURPOSE
+# Installs Ansible via pip. We should be in the virtualenv at this point.
+pip_install_ansible () {
+    echo "[LAPTOP] Installing ansible into the venv."
+    pip install --no-cache-dir --upgrade wheel ansible
+}
+
+run_playbook () {
+    ansible-pull -v -U https://github.com/${ORG_REPOS} playbook.yaml -v -i hosts
+}
+
+main () {
+    install_homebrew
+    install_git
+    setup_tmp_dir
+    get_pip
+    setup_virtual_environment
+    pip_install_ansible
+    run_playbook
+}
+
+main
